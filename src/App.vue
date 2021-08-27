94470143ab9b18f2fcadc4a2c784aa7b699561bc
Gonzalo López <thelupillo@hotmail.com>
2021-08-27 18:03:38 -0500

    Added the QrCodeScanner component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your QR Code Basic App"/>
+  <QrCodeScanner />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QrCodeScanner from './components/QrCodeScanner.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QrCodeScanner
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/QrCodeScanner.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <qr-stream class="qrstream"></qr-stream>
+  </div>
+</template>
+
+<script>
+import { QrStream } from 'vue3-qr-reader';
+
+export default {
+    name: "QrCodeScanner",
+    components: {
+        QrStream
+    }
+}
+</script>
+
+<style scoped>
+.qrstream {
+    height: 100vh;
+    width: 100vw;
+    margin: 0;
+    padding: 0;
+}
+</style>
